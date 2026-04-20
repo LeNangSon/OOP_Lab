a0450597e893cc92af24884ad86b89abf2969836
lenangson <Son.LN2417019@sis.hust.edu.vn>
2026-04-20 13:31:50 +0700

    done

--- AimsProject/src/General/Aims.java
+++ AimsProject/src/General/Aims.java
@@ -13,11 +13,7 @@ public class Aims {
         DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation",18.99f);
         anOrder.addDigitalVideoDisc(dvd3);
 
-        System.out.println("Total Cost is: ");
-        System.out.println(anOrder.totalCost());
-
-        anOrder.removeDigitalVideoDisc(dvd1);
-        anOrder.viewCart();
+        anOrder.viewCart2();
     }
 }
 
